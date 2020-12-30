c7033d59f0104bf8a7d7da36cd89a33b8f95c6f4
Vladimir Mukhin <vlad@mukhin.page>
2020-12-30 15:12:52 -0500

    Add basic controller

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 make install
 make docker-build docker-push IMG=public.ecr.aws/v6p5s4u6/s3controller:latest
 make deploy IMG=public.ecr.aws/v6p5s4u6/s3controller:latest
-kubectl -n tutorial-system scale deploy s3controller-controller-manager --replicas 0
-kubectl -n tutorial-system scale deploy s3controller-controller-manager --replicas 1
+kubectl -n s3controller-system scale deploy s3controller-controller-manager --replicas 0
+kubectl -n s3controller-system scale deploy s3controller-controller-manager --replicas 1

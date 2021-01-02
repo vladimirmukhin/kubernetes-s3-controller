55812a41ec5c4761ec95284d7e443603b484b447
Vladimir Mukhin <vlad@mukhin.page>
2021-01-02 18:57:18 -0500

    install.sh applies sample

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ make docker-build docker-push IMG=public.ecr.aws/v6p5s4u6/s3controller:latest
 make deploy IMG=public.ecr.aws/v6p5s4u6/s3controller:latest
 kubectl -n s3controller-system scale deploy s3controller-controller-manager --replicas 0
 kubectl -n s3controller-system scale deploy s3controller-controller-manager --replicas 1
+kubectl -n s3controller-system delete -f config/samples/awscloud_v1_s3bucket.yaml
+kubectl -n s3controller-system apply -f config/samples/awscloud_v1_s3bucket.yaml
